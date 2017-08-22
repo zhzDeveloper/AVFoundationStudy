1563ec1daae6ff99c33196ddc33dcf2630cf816a
zhaohuazhao <zhaohuazhao@yupaopao.cn>
2017-08-22 17:56:25 +0800

    start

---
+++ AVStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVStudy
+//
+//  Created by zhz on 22/08/2017.
+//  Copyright © 2017 zhz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
